01e67c175c65c6f643f43da65753d53a282daa5e
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-17 14:03:40 -0300

    conexão com banco mlab

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://heroku:1234@cluster0-jyyah.mongodb.net/goweek-backend?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.get('/', (req, res) => {
     return res.send('Hello World');
