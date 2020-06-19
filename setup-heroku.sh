8586b45ac49844ec8e84dead68e2ad13a2cb0528
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-19 17:12:13 -0300

    teste chamando heroku

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
 # If you need access to the heroku CLI to run heroku commands in the deploy step add these lines:
-mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
-ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+# mkdir -p /usr/local/lib /usr/local/bin
+# tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+# ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
