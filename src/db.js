ff8548751ca91b0393bd61ba644c4c67425230b9
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-19 17:44:43 -0300

    usando MONGODB_URI

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ const {
     MONGODB_URI
 } = process.env;
 
-const url = 'mongodb+srv://heroku:1234@cluster0-jyyah.mongodb.net/goweek-backend?retryWrites=true&w=majority' || `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
+const url = MONGODB_URI || `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
 
 const options = {
     useNewUrlParser: true,
