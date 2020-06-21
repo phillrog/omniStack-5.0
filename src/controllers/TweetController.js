1e4420819737b3b24bdd7327d7022b5c604469d3
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-21 14:12:26 -0300

    Remover autor nulos

--- src/controllers/TweetController.js
+++ src/controllers/TweetController.js
@@ -3,7 +3,7 @@ const Tweet = require('../models/Tweet');
 
 module.exports = {
     async index(req, res) {
-        const tweets = await Tweet.find().sort('-createdAt');
+        const tweets = await Tweet.find({'author': {$ne: null}}).sort('-createdAt');
 
         return res.json(tweets);
     },
